2c759a601779df171d58b1d5bdb1511056804d52
jordankageorgieva <jo_georgieva@abv.bg>
2025-03-02 11:09:45 +0200

    add new todo

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,84 +1,88 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoListItem from './TodoListItem';
+import './TodoList.css';
 
 export default function TodoList() {
-
     const [todos, setTodos] = useState([]);
-    const [isPending, setIsPending] = useState(true);
-    
-    console.log(todos);
+    const [newTodo, setNewTodo] = useState('');
+    const [isPending, setIsPending] = useState(false);
 
     useEffect(() => {
-        // const url = 'http://localhost:3030/jsonstore/todos';
-        // const url = 'http://localhost:3001/data/todos';
-        const url = 'https://todo-list-server-tau.vercel.app/data/todos';
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                const result = Object.values(data);
-                setTodos(result);
-                // when data is available already, set isPending to false
-                setIsPending(false);
-            })
-            .catch(err => console.log(err.message));
+        fetchTodos();
     }, []);
 
-    const buttonClickHandler = (_id) => {
-        
-        // HARD-CORE
-        // setTodos(oldTodo => oldTodo.map(todo => todo._id === _id ? { ...todo, isCompleted: !todo.isCompleted } : todo));
-        console.log('Button clicked with id: ' + _id);
-        handleUpdate(_id);
-        
-        // setTodos(oldTodos => oldTodos.map(todo => {
-        //     if (todo._id === _id) {
-        //         return { ...todo, isCompleted: !todo.isCompleted };
-        //     }
-        //     return todo;
-        // }));
-    }
-
-    const handleUpdate = async (id) => {
-        console.log('Button clicked with id: ' + id);
-        const todo = todos.find(x => x._id === id);
-        const response = await fetch(`https://todo-list-server-tau.vercel.app/data/todos/${id}`, {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                _id: id,
-                text: todo.text,
-                isCompleted: !todo.isCompleted
-            })
-        });
-
-        if (response.ok) {
-            const updatedTodo = await response.json();
-            setTodos(todos.map(x => x._id === id ? updatedTodo : x));
+    const fetchTodos = async () => {
+        setIsPending(true);
+        try {
+            const response = await fetch('http://localhost:3001/data/todos');
+            const data = await response.json();
+            setTodos(Object.values(data));
+        } catch (error) {
+            console.error('Error fetching todos:', error);
+        } finally {
+            setIsPending(false);
         }
-    }
+    };
+
+    const handleAddTodo = async (e) => {
+        e.preventDefault();
+        const newTodoItem = {
+            _id: `todo_${Date.now()}`,
+            text: newTodo,
+            isCompleted: false
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3001/data/todos/${newTodoItem._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTodoItem)
+            });
+
+            if (response.ok) {
+                setTodos([...todos, newTodoItem]);
+                setNewTodo('');
+            } else {
+                console.error('Failed to add new todo:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error adding new todo:', error);
+        }
+    };
+
+    const buttonClickHandler = (_id) => {
+        // Implement the button click handler logic here
+    };
 
     return (
         <>
-            {/* <!-- Section container --> */}
             <section className="todo-list-container">
                 <h1>Todo List</h1>
 
                 <div className="add-btn-container">
-                    <button className="btn">+ Add new Todo</button>
+                    <form onSubmit={handleAddTodo}>
+                        <input
+                            type="text"
+                            value={newTodo}
+                            onChange={(e) => setNewTodo(e.target.value)}
+                            placeholder="Add new todo"
+                            required
+                        />
+                        <button type="submit" className="btn">+ Add new Todo</button>
+                    </form>
                 </div>
 
                 <div className="table-wrapper">
-
-                    {/* <!-- Loading spinner - show the load spinner when fetching the data from the server--> */}
-                    {isPending &&
+                    {isPending && (
                         <div className="loading-container">
                             <div className="loading-spinner">
                                 <span className="loading-spinner-text">Loading</span>
                             </div>
                         </div>
-                    }
+                    )}
 
-                    {/* <!-- Todo list table --> */}
                     <table className="table">
                         <thead>
                             <tr>
@@ -88,8 +92,7 @@ export default function TodoList() {
                             </tr>
                         </thead>
                         <tbody>
-
-                            {todos.map(todo =>
+                            {todos.map(todo => (
                                 <TodoListItem
                                     key={todo._id}
                                     _id={todo._id}
@@ -97,39 +100,7 @@ export default function TodoList() {
                                     isCompleted={todo.isCompleted}
                                     buttonClickHandler={buttonClickHandler}
                                 />
-                            )}
-
-                            {/* <TodoListItem />
-
-                            <TodoListItem /> */}
-
-                            {/* <!-- Todo item -->
-                            <tr className="todo is-completed">
-                                <td>Feed cat</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr> */}
-
-                            {/* <!-- Todo item --> */}
-                            {/* <tr className="todo">
-                                <td>Change light bulbs</td>
-                                <td>Incomplete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr> */}
-
-                            {/* <!-- Todo item --> */}
-                            {/* <tr className="todo is-completed">
-                                <td>Feed cat</td>
-                                <td>Complete</td>
-                                <td className="todo-action">
-                                    <button className="btn todo-btn">Change status</button>
-                                </td>
-                            </tr> */}
-
+                            ))}
                         </tbody>
                     </table>
                 </div>
